19d02159fa6599c0ef7f6a4163a12d2a8adf5c99
Vaibhav Kulkarni <kulkarni.vaibhav18298@gmail.com>
2019-06-29 16:02:49 +0530

    oddeven parallel algorithm implemented

--- HPC2/HPC2.cpp
+++ HPC2/HPC2.cpp
@@ -1,21 +1,21 @@
 #include<iostream>
+#include<omp.h>
+#include<stdlib.h>
+#define MAX 50
 using namespace std ;
 void disp( int* a )
 {
-	for(int i = 0 ; i < 6 ; i++)
+	for(int i = 0 ; i < MAX ; i++)
 	{
 		cout<<a[i]<<" " ; 
 	}
 	cout<<"\n";
 }
-int main()
-{
-	int a[6] ={5,4,3,2,1,0}, n = 0 , temp = 0;
-	cout<<" before sorting "<<endl;
-	disp(a);
-	for(int i = 0 ; i < 6 ; i++)
+void bubblesort( int* a)
+{	int temp = 0;
+	for(int i = 0 ; i < MAX ; i++)
 	{
-		for(int j = 0 ; j < 5; j++)
+		for(int j = 0 ; j < MAX -1; j++)
 		{
 			if(a[j] > a[j+1])
 			{
@@ -25,6 +25,42 @@ int main()
 			}
 		}
 	}
+}
+void oddeven_bubblesort(int* a)
+{
+	
+	for(int i = 0 ; i < MAX ; i++)
+	{
+		//odd phase
+		//#pragma omp parallel for
+		for(int j = 1 ; j < MAX-1 ; j+=2)
+		{
+			if(a[j] > a[j+1])
+			{
+				swap(a[j] , a[j+1]);
+			}
+		}
+		//even phase
+		//#pragma omp parallel for
+		for(int k = 0 ; k < MAX -1 ; k+=2)
+		{
+			if(a[k] > a[k+1])
+			{
+				swap(a[k] , a[k+1]);
+			}
+		}
+	}
+}
+int main()
+{
+	int a[50];
+	for(int i = 0 ; i < MAX ; i++)
+	{
+		a[i] = rand()%1000;
+	}
+	cout<<" before sorting "<<endl;
+	disp(a);
+	oddeven_bubblesort(a);
 	cout<<"after sorting " <<endl;
 	disp(a);
 	
